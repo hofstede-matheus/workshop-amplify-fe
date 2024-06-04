a05b6019f7e673883a5458c94d3de35419ef4531
hofstede-matheus <hofstede.matheus@gmail.com>
2024-06-04 20:09:57 -0300

    fix typo

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          CHange <code>src/App.tsx</code> and save to test HMR
+          Change <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
